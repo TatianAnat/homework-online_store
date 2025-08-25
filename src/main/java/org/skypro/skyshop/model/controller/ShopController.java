82dc0b8f36556d660625db4b7eb41b6882e37503
TatianAnat <TatianAnat@yandex.ru>
2025-08-26 00:21:57 +0400

    homework shop_paragraph3_BasketService

--- src/main/java/org/skypro/skyshop/model/controller/ShopController.java
+++ src/main/java/org/skypro/skyshop/model/controller/ShopController.java
@@ -1,16 +1,16 @@
 package org.skypro.skyshop.model.controller;
 
 import org.skypro.skyshop.model.article.Article;
+import org.skypro.skyshop.model.basket.UserBasket;
 import org.skypro.skyshop.model.product.Product;
 import org.skypro.skyshop.model.search.SearchResult;
+import org.skypro.skyshop.model.service.BasketService;
 import org.skypro.skyshop.model.service.SearchService;
 import org.skypro.skyshop.model.service.StorageService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
+import java.util.UUID;
 
 @RequestMapping("skyshop")
 @RestController
@@ -18,10 +18,12 @@ public class ShopController {
 
     private final SearchService searchService;
     private final StorageService storageService;
+    private final BasketService basketService;
 
-    public ShopController(StorageService storageService,SearchService searchService) {
+    public ShopController(StorageService storageService,SearchService searchService, BasketService basketService) {
         this.storageService = storageService;
         this.searchService = searchService;
+        this.basketService = basketService;
     }
 
     @GetMapping("/products")
@@ -37,5 +39,20 @@ public class ShopController {
     @GetMapping("/search")
     public Collection<SearchResult> search(@RequestParam("pattern") String pattern) {
         return searchService.search(pattern);
+    }
+    /**
+     * Метод добавления продукта в корзину
+     */
+    @GetMapping("/basket/{id}")
+    public String addProduct(@PathVariable("id")UUID id) {
+        basketService.addProductBasket(id);
+        return "Продукт успешно добавлен";
+    }
+    /**
+     * Метод отображения текущего состояния корзины
+     */
+    @GetMapping("/basket")
+    public UserBasket getUserBasket() {
+        return basketService.getUserBasket();
     }
 }
