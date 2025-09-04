dcec013698e1705791d97e593f5e9b3f8759505b
TatianAnat <TatianAnat@yandex.ru>
2025-09-04 21:28:32 +0400

    homework shop_paragraph1_SpringBoot

---
+++ src/main/java/org/skypro/skyshop/model/exceptions/NoSuchProductException.java
@@ -0,0 +1,20 @@
+package org.skypro.skyshop.model.exceptions;
+
+public class NoSuchProductException extends RuntimeException {
+
+    public NoSuchProductException() {
+        super();
+    }
+
+    public NoSuchProductException(String message) {
+        super(message);
+    }
+
+    public NoSuchProductException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NoSuchProductException(Throwable cause) {
+        super(cause);
+    }
+}
